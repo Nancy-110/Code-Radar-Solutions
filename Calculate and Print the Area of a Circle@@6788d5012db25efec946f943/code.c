301da0a7d2363d8dc9cb6d928a57c59aafb0ea71
Nancy-110 <nancy555chauhan@gmail.com>
2025-02-02 18:31:05 +0530

    Solution to Calculate and Print the Area of a Circle

--- Calculate and Print the Area of a Circle@@6788d5012db25efec946f943/code.c
+++ Calculate and Print the Area of a Circle@@6788d5012db25efec946f943/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
-    int r, area;
+    float r, area;
     scanf("%d",&r);
     area = 3.14*r*r;
-    printf("Area: %d",area);
+    printf("Area: %.2f",area);
     return 0;
 
 }
