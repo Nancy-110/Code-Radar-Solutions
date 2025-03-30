840e6ba94fa02eddcbc4530611186e149e4cf34e
Nancy chauhan <nancy555chauhan@gmail.com>
2025-03-30 15:48:12 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,36 +1,54 @@
 #include <stdio.h>
-int firstPeak(int arr[],int n);
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i = 0; i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int FirstPeak = firstPeak(arr, n);
-    
-    if (FirstPeak != -1) {
-        printf(" %d\n",firstPeak);
-    } else {
-        printf("No peak element found in the array.\n");
-    }
-    
-    return 0;
-}
-int firstPeak(int arr[],int n){
-    if(n==1){
+
+// Function to find the first peak element in an array
+int findFirstPeak(int arr[], int n) {
+    // If array has only one element
+    if (n == 1) {
         return 0;
     }
-    if(arr[0]>=arr[1]){
+    
+    // Check first element (only needs to be greater than right neighbor)
+    if (arr[0] >= arr[1]) {
         return 0;
     }
-    for(int i =1;i<n-1;i++){
-        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+    
+    // Check middle elements
+    for (int i = 1; i < n - 1; i++) {
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
             return i;
         }
     }
-    if(arr[n-1]>arr[n-2]){
-        return n-2;
+    
+    // Check last element (only needs to be greater than left neighbor)
+    if (arr[n - 1] >= arr[n - 2]) {
+        return n - 1;
     }
+    
+    // If no peak found (all elements are equal)
     return -1;
+}
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    
+    if (n <= 0) {
+        return 1;
+    }
+    
+    int arr[n];
+    
+    printf("%d", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int peakIndex = findFirstPeak(arr, n);
+    
+    if (peakIndex != -1) {
+        printf("%d\n", arr[peakIndex]);
+    } 
+    
+    return 0;
 }
