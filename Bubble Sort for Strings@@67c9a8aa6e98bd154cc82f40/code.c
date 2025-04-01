e1cf07317e86b22f8782ce9e1105b77e87692392
Nancy chauhan <nancy555chauhan@gmail.com>
2025-04-01 10:29:17 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+
+// Function to swap two strings
+void swap(char str1[], char str2[]) {
+    char temp[100]; // Temporary array to hold one of the strings
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
+}
+
+// Bubble Sort function for strings
 void bubbleSort(char arr[][100], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {  // Swap if the element is greater
-                char temp[100] = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp[100];
+            if (strcmp(arr[j], arr[j + 1]) > 0) {
+                // Swap strings if they are in the wrong order
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
-// Function to print an array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+// Function to print an array of strings
+void printArray(char arr[][100], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%s\n", arr[i]);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of strings: ");
+    scanf("%d", &n);
+    char arr[n][100];
+
+    printf("Enter the strings:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
+    }
+
+    bubbleSort(arr, n); // Sort the strings
+
+    printf("Sorted strings:\n");
+    printArray(arr, n); // Print sorted strings
+
+    return 0;
 }
