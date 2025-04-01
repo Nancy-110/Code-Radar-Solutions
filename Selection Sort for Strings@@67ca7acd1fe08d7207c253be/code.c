d7d2dccdcc26d0939b909a1ad377b51799bcfc40
Nancy chauhan <nancy555chauhan@gmail.com>
2025-04-01 13:22:15 +0530

    Solution to Selection Sort for Strings

---
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to swap two elements
+void swap(char *a[100], char *b[100]) {
+    char[temp] = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to perform Selection Sort
+void selectionSort(char arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int small = i; // Assume the first element is the smallest
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[small]) {
+                small = j; // Update the index of the smallest element
+            }
+        }
+        // Swap the smallest element with the first element of the unsorted part
+        swap(&arr[i], &arr[small]);
+    }
+}
+
+// Function to print the array
+void printArray(char arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%c ", arr[i]);
+    }
+    printf("\n");
+}
