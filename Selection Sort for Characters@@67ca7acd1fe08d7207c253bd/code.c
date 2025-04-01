8e81081ba4ee12319340be7bed1a8d5432ac6ae0
Nancy chauhan <nancy555chauhan@gmail.com>
2025-04-01 09:43:49 +0530

    Solution to Selection Sort for Characters

---
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to perform Selection Sort
+void selectionSort(char arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i; // Assume the first element is the smallest
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j; // Update the index of the smallest element
+            }
+        }
+        // Swap the smallest element with the first element of the unsorted part
+        swap(&arr[i], &arr[minIndex]);
+    }
+}
+
+// Function to print the array
+void printArray(char arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%c ", arr[i]);
+    }
+    printf("\n");
+}
